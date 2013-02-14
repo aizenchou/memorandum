75a68cfa885f9d243f77cbbfee51fc8510cb9299
aizenchou <zhoujiajun5589@gmail.com>
2013-02-14 10:10:31 +0800

    first git about memorandum--Dymanic Clock

---
+++ src/Memorandum.java
@@ -0,0 +1,170 @@
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Timer;
+import java.util.TimerTask;
+import javax.swing.*;
+
+
+public class Memorandum extends JFrame implements ActionListener {
+	private JLabel dateLabel;
+	private JPanel timePanel;
+	private JLabel displayArea;
+	private String DEFAULT_TIME_FORMAT = "HH:mm:ss";
+	private String time;
+	private int ONE_SECOND = 1000;
+	private JButton setMemButton;
+	private JButton minimizeButton;
+
+	public Memorandum() {
+		timePanel = new JPanel(null);
+		displayArea = new JLabel();
+		dateLabel = new JLabel();
+		setMemButton = new JButton("编辑备忘录");
+		minimizeButton = new JButton("最小化至托盘");
+
+		configTimeArea();
+
+		dateLabel.setText(getDate());
+		dateLabel.setFont(new Font("微软雅黑", Font.PLAIN, 16));
+		displayArea.setFont(new Font("TimesRoman", Font.PLAIN, 40));
+		minimizeButton.setBounds(150, 100, 130, 30);
+		setMemButton.setBounds(5, 100, 130, 30);
+		dateLabel.setBounds(0, 0, 300, 50);
+		displayArea.setBounds(0, 15, 300, 100);
+		dateLabel.setHorizontalAlignment(SwingConstants.CENTER);
+		displayArea.setHorizontalAlignment(SwingConstants.CENTER);
+		setMemButton.addActionListener(this);
+		minimizeButton.addActionListener(this);
+		timePanel.add(dateLabel);
+		timePanel.add(displayArea);
+		timePanel.add(setMemButton);
+		timePanel.add(minimizeButton);
+		this.add(timePanel);
+		this.setTitle("日历备忘录");
+		this.setSize(300, 250);
+		this.setLocationRelativeTo(null);
+		this.setTray();
+		Image image1 = Toolkit.getDefaultToolkit().getImage(
+				getClass().getResource("/trayIconImage/image.png"));
+		setIconImage(image1);
+		this.setVisible(true);
+	}
+
+	private void configTimeArea() {
+		Timer tmr = new Timer();
+		tmr.scheduleAtFixedRate(new JLabelTimerTask(), new Date(), ONE_SECOND);
+	}
+
+	protected class JLabelTimerTask extends TimerTask {
+		SimpleDateFormat dateFormatter = new SimpleDateFormat(
+				DEFAULT_TIME_FORMAT);
+
+		@Override
+		public void run() {
+			time = dateFormatter.format(Calendar.getInstance().getTime());
+			displayArea.setText(time);
+		}
+	}
+
+	public String getDate() {
+		int y, m, d;
+		Calendar cal = Calendar.getInstance();
+		y = cal.get(Calendar.YEAR);
+		m = cal.get(Calendar.MONTH);
+		d = cal.get(Calendar.DATE);
+		return y + "年" + m + "月" + d + "日";
+	}
+
+	// 添加托盘显示：1.先判断当前平台是否支持托盘显示
+	public void setTray() {
+
+		if (SystemTray.isSupported()) {// 判断当前平台是否支持托盘功能
+			// 创建托盘实例
+			SystemTray tray = SystemTray.getSystemTray();
+			// 创建托盘图标：1.显示图标Image 2.停留提示text 3.弹出菜单popupMenu 4.创建托盘图标实例
+			// 1.创建Image图像
+			Image image = Toolkit.getDefaultToolkit().getImage(
+					getClass().getResource("/trayIconImage/clientIcon.gif"));
+			// 2.停留提示text
+			String text = "日历备忘录";
+			// 3.弹出菜单popupMenu
+			PopupMenu popMenu = new PopupMenu();
+			MenuItem itmOpen = new MenuItem("打开");
+			itmOpen.addActionListener(new ActionListener() {
+				public void actionPerformed(ActionEvent e) {
+					Show();
+				}
+			});
+			MenuItem itmHide = new MenuItem("隐藏");
+			itmHide.addActionListener(new ActionListener() {
+				public void actionPerformed(ActionEvent e) {
+					UnVisible();
+				}
+			});
+			MenuItem itmExit = new MenuItem("退出");
+			itmExit.addActionListener(new ActionListener() {
+				public void actionPerformed(ActionEvent e) {
+					Exit();
+				}
+			});
+			MenuItem itmEdit = new MenuItem("编辑");
+			itmEdit.addActionListener(new ActionListener() {
+				public void actionPerformed(ActionEvent e) {
+					Edit();
+				}
+			});
+			popMenu.add(itmOpen);
+			popMenu.add(itmHide);
+			popMenu.add(itmEdit);
+			popMenu.add(itmExit);
+
+			// 创建托盘图标
+			TrayIcon trayIcon = new TrayIcon(image, text, popMenu);
+			// 将托盘图标加到托盘上
+			try {
+				tray.add(trayIcon);
+			} catch (AWTException e1) {
+				e1.printStackTrace();
+			}
+		}
+	}
+
+	// 内部类中不能直接调用外部类（this不能指向）
+	public void UnVisible() {
+		this.setVisible(false);
+	}
+
+	public void Show() {
+		this.setVisible(true);
+	}
+
+	public void Edit() {
+		DateChooser.setmem();
+	}
+	public void Exit() {
+		System.exit(0);
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO 自动生成的方法存根
+		Object obj = e.getSource();
+		if(obj == setMemButton)
+		{
+			DateChooser.setmem();
+		}
+		else if(obj == minimizeButton)
+		{
+			this.setVisible(false);
+		}
+	}
+
+	public static void main(String[] args) {
+		new Memorandum();
+	}
+
+}
